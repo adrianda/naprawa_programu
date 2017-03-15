58f51ec9e349735be6561972aec9c7e33af97c66
Adrian Dawid <adriandawid1@gmail.com>
2017-03-15 16:36:25 +0100

    poprawiony_program

---
+++ L.Samluk_08_15.03.2017/zad6_2.cpp
@@ -0,0 +1,36 @@
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+ 
+int main()
+{
+    ifstream dane("dane_6_2.txt");
+    ofstream wypis("wyniki_6_2.txt");
+    string slowo;
+    int a, k;
+    for (int i = 0; i < 3000; ++i)
+    {
+        dane >> slowo;
+        dane >> k;
+        for (int j = 0; j < slowo.size(); ++j)
+        {
+            a = slowo[j];
+            for (int l = 0; l < k; ++l)             // petla deszyfrujaca
+            {
+                if (a > 65)
+                    a--;
+                else
+                    a = 90;                         // jesli dojdzie do A przeskakuje do Z
+            }
+            slowo[j] = a;
+        }
+        wypis << slowo << endl;
+    }
+    dane.close();
+    wypis.close();
+    return 0;
+}
+
